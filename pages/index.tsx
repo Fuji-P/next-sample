2ba754cde2bab3d58b68653fc04f50b5682f2ade
Fuji-P <nyaguri0417@gmail.com>
2022-11-15 00:20:45 +0900

    4 コンポーネント開発 4.2.1/styled-componentsをNext.jsに導入

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
+import type { NextPage } from "next";
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import styled from "styled-components";
 
-export default function Home() {
+const H1 = styled.h1`
+  color: red;
+`;
+
+const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -12,9 +18,9 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <H1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        </H1>
 
         <p className={styles.description}>
           Get started by editing{' '}
@@ -69,3 +75,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home;
